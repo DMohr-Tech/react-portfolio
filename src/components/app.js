620fff02600cab6d58310fc9595c0cfeb350dc5b
DMohr-Tech <DMohr-Tech@users.noreply.github.com>
2020-08-01 18:45:44 -0400

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>The Dom Port</h1>
+        <h2>Dun Dun DU n dUN</h2>
+        <div>
+        </div>
+      </div>
+    );
+  }
+}
